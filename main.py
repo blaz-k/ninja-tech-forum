4d842890110e38415408e52d9e8fbabd4e1f9078
blaz <blazyy@gmail.com>
2021-06-22 12:22:49 +0200

    added edit_profile and some small corrections

--- main.py
+++ main.py
@@ -38,6 +38,43 @@ app.add_url_rule(rule="/topic/<topic_id>", endpoint="topic.topic_details", view_
 # USER
 
 
+@app.route("/dashboard/edit-profile", methods=["GET", "POST"])
+def edit_profile():
+    session_cookie = request.cookies.get("session")
+
+    if session_cookie:
+        user = db.query(User).filter_by(session_token=session_cookie).first()
+
+        if not user:
+            return render_template("error.html")
+
+    else:
+        return render_template("error.html")
+
+    if request.method == "GET":
+        return render_template("edit-profile.html", user=user)
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        first_name = request.form.get("first-name")
+        last_name = request.form.get("last-name")
+        email = request.form.get("user-email")
+        telephone = request.form.get("telephone")
+        password = request.form.get("password")
+        repeat = request.form.get("repeat")
+
+        user.username = username
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        user.telephone = telephone
+        user.password = sha256(password.encode("utf-8")).hexdigest()
+        user.repeat = repeat
+        user.save()
+
+        return redirect(url_for("dashboard"))
+
+
 @app.route("/dashboard", methods=["GET", "POST"])
 def dashboard():
 
