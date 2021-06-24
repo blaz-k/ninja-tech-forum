5583ca35a3d0259adbb0c0efea3876cf3f163b59
blaz <blazyy@gmail.com>
2021-06-24 09:53:00 +0200

    showing comments for topics

--- handlers/comment.py
+++ handlers/comment.py
@@ -12,6 +12,7 @@ def user_comment(topic_id):
     topic = db.query(Topic).get(int(topic_id))
     session_cookie = request.cookies.get("session")
 
+
     user = None
     if session_cookie:
         user = db.query(User).filter_by(session_token=session_cookie).first()
@@ -20,6 +21,7 @@ def user_comment(topic_id):
 
         new_comment_content = Comment(content=comment_content, topic=topic, author=user)
         new_comment_content.save()
+
 
         return redirect(url_for("topic.topic_details", topic_id=topic_id))
 
--- handlers/topic.py
+++ handlers/topic.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for
 
 from models.topic import Topic
 from models.user import User
+from models.comment import Comment
 from models.settings import db
 
 
@@ -40,25 +41,17 @@ def topic_create():
 def topic_details(topic_id):
     topic = db.query(Topic).get(int(topic_id))
     session_cookie = request.cookies.get("session")
+    comments = db.query(Comment).all()
+
     user = None
     if session_cookie:
         user = db.query(User).filter_by(session_token=session_cookie).first()
     topics =db.query(Topic).all()
 
-    if request.method == "GET":
-        if not topic:
-            return render_template("/response/not-found.html", user=user)
+    if not topic:
+        return render_template("/response/not-found.html", user=user)
 
-        return render_template("topic/topic.html", topic=topic, topics=topics, user=user)
-"""
-    elif request.method == "POST":
-        comment_content = request.form.get("content")
-
-        new_comment_content = Comment(content=comment_content, topic=topic, author=user)
-        new_comment_content.save()
-
-        return redirect(url_for("topic.topic_details", topic_id=topic_id))
-"""
+    return render_template("topic/topic.html", topic=topic, topics=topics, user=user, comments=comments)
 
 
 
