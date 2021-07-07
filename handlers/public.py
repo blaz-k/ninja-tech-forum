24c09f672e8d298e8229d8891a05b41624e85b0c
blaz <blazyy@gmail.com>
2021-07-07 08:40:15 +0200

    no message

--- handlers/public.py
+++ handlers/public.py
@@ -21,24 +21,28 @@ def home():
 def contact():
     session_cookie = request.cookies.get("session")
 
-    if session_cookie:
-        user = db.query(User).filter_by(session_token=session_cookie).first()
-        if user:
-            return render_template("contact.html", user=user)
+    if request.method == "GET":
 
-        if request.method == "GET":
+        if session_cookie:
+            user = db.query(User).filter_by(session_token=session_cookie).first()
+            if user:
+                return render_template("contact.html", user=user)
+        return render_template("contact.html")
 
-            return render_template("contact.html", user=user)
+    elif request.method == "POST":
 
-        elif request.method == "POST":
+        name = request.form.get("name")
+        sender_email = request.form.get("email")
+        subject = request.form.get("subject")
+        message = request.form.get("message")
 
-            name = request.form.get("name")
-            sender_email = request.form.get("email")
-            subject = request.form.get("subject")
-            message = request.form.get("message")
+        if session_cookie:
+            user = db.query(User).filter_by(session_token=session_cookie).first()
+            if user:
+                return render_template("contact.html", user=user)
+            return render_template("contact.html")
+        send_email(recipient="blazyy@gmail.com", subject=subject, user_name=name, body=message)
 
-            send_email(recipient="blazyy@gmail.com", subject=subject, user_name=name, body=message)
-
-        return render_template("/response/successful-message.html", user=user)
+        return render_template("/response/successful-message.html")
 
 
