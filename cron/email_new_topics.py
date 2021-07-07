eb8420eee5fccf1065351f945a9a687d9dd3519c
blaz <blazyy@gmail.com>
2021-07-07 12:13:44 +0200

    new topics message if today is monday

---
+++ cron/email_new_topics.py
@@ -0,0 +1,37 @@
+import datetime
+
+from models.settings import db
+from models.topic import Topic
+from models.user import User
+from utils import send_email
+
+
+def new_topics_email():
+    today = datetime.datetime.now()
+    monday = datetime.date.isoweekday(today) == 1
+
+    if today == monday:
+        yesterday_topics = db.query(Topic).filter(Topic.created > (datetime.datetime.now() - datetime.timedelta(days=1))).all()
+
+        # if no topics, finish the task without sending the email
+        if not yesterday_topics:
+            print("No new topics yesterday")
+        else:
+            message = "Topics created yesterday:\n"
+
+            for topic in yesterday_topics:
+                message += "{}\n".format(topic.title)
+
+                print(message)
+
+            users = db.query(User).all()
+
+            for user in users:
+                if user.email:
+                    send_email(recipient=user.email, subject="New topic(s) to check!", body=message)
+    else:
+        print("No new topics at his point")
+
+
+if __name__ == '__main__':
+    new_topics_email()
