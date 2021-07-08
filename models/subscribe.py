9eaea053561bcf6f0ea383b8d4cf02be0f196dd0
blaz <blazyy@gmail.com>
2021-07-08 14:28:51 +0200

    subscribe.py added

---
+++ models/subscribe.py
@@ -0,0 +1,12 @@
+from models.settings import db
+from datetime import datetime
+
+
+class Subscribe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship("User")
+    topic_id = db.Column(db.Integer, db.ForeignKey('topics.id'))
+    topic = db.relationship("Topic")
+    date = db.Column(db.DateTime, default=datetime.now())
+g
